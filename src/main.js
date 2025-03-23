2700e2c3748c34c8ad929fbf5c1ef9fa9049bb39
Jae <jaeyeonjung3@gmail.com>
2025-03-23 12:55:41 +0900

    [Style] 선택된 nav 색상 변경

--- src/main.js
+++ src/main.js
@@ -2,10 +2,11 @@
 const Header = () => {
   const user = JSON.parse(localStorage.getItem("user"));
   const isLoggedIn = user ? true : false;
+  const currentPath = window.location.pathname;
   const loggedInNav = isLoggedIn
-    ? `<li><a href="/profile" class="text-gray-600">프로필</a></li>
-       <li><a href="#" class="logout-button text-gray-600">로그아웃</a></li>`
-    : `<li><a href="/login" class="text-gray-600">로그인</a></li>`;
+    ? `<li><a href="/profile" class="${currentPath === "/profile" ? "text-blue-600" : "text-gray-600"}">프로필</a></li>
+       <li><a href="#" class="logout-button">로그아웃</a></li>`
+    : `<li><a href="/login" class="${currentPath === "/login" ? "text-blue-600" : "text-gray-600"}">로그인</a></li>`;
   return `
     <header class="bg-blue-600 text-white p-4 sticky top-0">
       <h1 class="text-2xl font-bold">항해플러스</h1>
@@ -13,7 +14,7 @@ const Header = () => {
 
     <nav class="bg-white shadow-md p-2 sticky top-14">
       <ul class="flex justify-around">
-        <li><a href="/" class="text-blue-600">홈</a></li>
+        <li><a href="/" class="${currentPath === "/" ? "text-blue-600" : "text-gray-600"}">홈</a></li>
         ${loggedInNav}
       </ul>
     </nav>
