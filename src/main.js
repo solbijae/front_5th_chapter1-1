dad503d9b15328329870faf1200109f0b6e1d347
Jae <jaeyeonjung3@gmail.com>
2025-03-23 12:21:37 +0900

    [Feat] Nav UI 수정: isLoggedIn에 따라 UI 제공

--- src/main.js
+++ src/main.js
@@ -1,17 +1,25 @@
 // Header
-const Header = () => `
-  <header class="bg-blue-600 text-white p-4 sticky top-0">
-    <h1 class="text-2xl font-bold">항해플러스</h1>
-  </header>
+const Header = () => {
+  const user = JSON.parse(localStorage.getItem("user"));
+  console.log(user);
+  const isLoggedIn = user ? true : false;
+  const loggedInNav = isLoggedIn
+    ? `<li><a href="/profile" class="text-gray-600">프로필</a></li>
+       <li><a href="#" class="text-gray-600">로그아웃</a></li>`
+    : `<li><a href="/login" class="text-gray-600">로그인</a></li>`;
+  return `
+    <header class="bg-blue-600 text-white p-4 sticky top-0">
+      <h1 class="text-2xl font-bold">항해플러스</h1>
+    </header>
 
-  <nav class="bg-white shadow-md p-2 sticky top-14">
-    <ul class="flex justify-around">
-      <li><a href="/" class="text-blue-600">홈</a></li>
-      <li><a href="/profile" class="text-gray-600">프로필</a></li>
-      <li><a href="#" class="text-gray-600">로그아웃</a></li>
-    </ul>
-  </nav>
-`;
+    <nav class="bg-white shadow-md p-2 sticky top-14">
+      <ul class="flex justify-around">
+        <li><a href="/" class="text-blue-600">홈</a></li>
+        ${loggedInNav}
+      </ul>
+    </nav>
+  `;
+};
 // Footer
 const Footer = () => `
   <footer class="bg-gray-200 p-4 text-center">
