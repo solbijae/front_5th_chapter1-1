42dc4b3e818eb693d7b327b8a1ee28b748c4bb23
Jae <jaeyeonjung3@gmail.com>
2025-03-25 21:38:20 +0900

    [Chore] 테스트/빌드 index 분기 처리

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from "vitest/config";
+import { resolve } from "path";
 
 export default defineConfig({
   test: {
@@ -7,5 +8,13 @@ export default defineConfig({
     setupFiles: "./src/setupTests.js",
     exclude: ["**/e2e/**", "**/*.e2e.spec.js", "**/node_modules/**"],
     base: "/front_5th_chapter1-1/",
+  },
+  build: {
+    rollupOptions: {
+      input:
+        process.env.NODE_ENV === "test"
+          ? resolve(__dirname, "index.html")
+          : resolve(__dirname, "index.hash.html"),
+    },
   },
 });
