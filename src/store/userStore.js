bb42aacf49c9f7b69fe50ad4bd9a8fbc54507d71
Jae <jaeyeonjung3@gmail.com>
2025-03-27 21:17:28 +0900

    [Feat] user 상태 관련 코드에 옵져버 패턴 추가

--- src/store/userStore.js
+++ src/store/userStore.js
@@ -1,24 +1,41 @@
 const userStore = {
   state: {
-    user: JSON.parse(localStorage.getItem("user") || null),
+    user: JSON.parse(localStorage.getItem("user") || "null"),
   },
+  observers: new Set(),
 
   getUser() {
     return this.state.user;
   },
 
   setUser(user) {
-    this.state.user = user;
     localStorage.setItem("user", JSON.stringify(user));
+    this.state.user = user;
+    this.notify();
+    return user;
   },
 
   clearUser() {
-    this.state.user = null;
     localStorage.removeItem("user");
+    this.state.user = null;
+    this.notify();
+    return null;
   },
 
   isLoggedIn() {
     return !!this.state.user;
+  },
+
+  subscribe(observer) {
+    this.observers.add(observer);
+  },
+
+  unsubscribe(observer) {
+    this.observers.delete(observer);
+  },
+
+  notify() {
+    this.observers.forEach((observer) => observer(this.state.user));
   },
 };
 
